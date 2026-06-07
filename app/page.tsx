13d2b05b42492171e8ed6d68877dab6ef9ef9d32
unknown <bimasakti.kr@gmail.com>
2026-06-07 13:34:55 +0700

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,9 @@ function PortalGatewayContent() {
     else if (id === 'ASYNC_2') router.push('?modul=Async2')
     else if (id === 'PENDALAMAN') router.push('?modul=Pendalaman');
     else if (id === 'POST_TEST') window.open('https://forms.gle/DmRrPEmDEfmWQtkC6', '_blank');
-    else if (id === 'PRETEST') window.open('https://docs.google.com/forms/d/e/1FAIpQLSeI7zHMFrEEtF7GgrFuX1GojTWqQAbg1H1mA2ySEF1i7CRxOA/viewform', '_blank');
+    else if (id === 'PRETEST') 
+        window.open('https://docs.google.com/forms/d/e/1FAIpQLSeI7zHMFrEEtF7GgrFuX1GojTWqQAbg1H1mA2ySEF1i7CRxOA/viewform', '_blank');
+      
     else if (id === 'EVALUASI') router.push('?modul=evaluasi'); // 🟢 RUTE BARU
     // 🟢 TAMBAHKAN INI (Hanya untuk Admin/Instruktur yang tahu link-nya)
     else if (id === 'ADMIN_EVALUASI') router.push('?modul=adminevaluasi');
@@ -171,7 +173,18 @@ function PortalGatewayContent() {
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>
                 Login Portal Instruktur
               </button>
+
+              {/* 🟢 TOMBOL LOGIN EVALUASI BARU */}
+              <button 
+                onClick={() => router.push('?modul=PortalEvaluasiLogin')}
+                className="bg-emerald-600 hover:bg-emerald-500 text-white font-bold px-6 py-3 rounded-lg shadow-lg transition flex items-center gap-2"
+              >
+                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/></svg>
+                Login Evaluasi
+              </button>
             </div>
+
+            
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -221,6 +234,7 @@ function PortalGatewayContent() {
             </div>
           </div>
         </div>
+
         <div className="flex items-center gap-4">
           <button
             onClick={handleForceRefreshConfig}
